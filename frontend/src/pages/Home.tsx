5eb821e7c531163580f9b80a95d89f8121e58154
tajima-yasunari <135994628+tajima-yasunari@users.noreply.github.com>
2025-08-07 18:55:43 +0900

    基盤整えた

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,10 +6,22 @@ const Home = () => {
   const goToEvents = () => {
     navigate('/events');
   }
+
+  const goToTalk = () => {
+    navigate('/talk')
+  }
+
+  const goToSchedule = () => {
+    navigate('/schedule')
+  }
+
+
   return (
     <div>
       <h1>ようこそ SyncPlan-AI へ！</h1>
-      <button onClick={goToEvents}>イベントページへ</button>
+      <button onClick={goToEvents}>イベント掲載ページへ</button>
+      <button onClick={goToTalk}>AIとの対話ページへ</button>
+      <button onClick={goToSchedule}>スケジュール一覧ページへ</button>
     </div>
     
   );
--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Events from "../pages/Events";
+import Talk from '../pages/Talk';
+import Schedule from '../pages/Schedule';
 
 const AppRouter = () => {
   return (
@@ -8,6 +10,8 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
+        <Route path="/talk" element={<Talk />} />
+        <Route path="/schedule" element={<Schedule />} />
         {/* ここにLoginPageやDashboardPageなど追加予定 */}
       </Routes>
     </Router>
